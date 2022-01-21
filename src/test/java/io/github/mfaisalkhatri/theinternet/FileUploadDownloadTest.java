b7d5ca08f2d3927b762e7375251cd25484e38337
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2022-01-21 22:44:58 +0530

    Added File upload tests (#49)
    * Added File upload tests
    
    * page added
    
    * skipped double click and contact us tests as website is down, corrected upload path separtor

---
+++ src/test/java/io/github/mfaisalkhatri/theinternet/FileUploadDownloadTest.java
@@ -0,0 +1,40 @@
+package io.github.mfaisalkhatri.theinternet;
+
+import io.github.mfaisalkhatri.driversetup.Setup;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class FileUploadDownloadTest extends Setup {
+
+    private String fileName;
+    private DownloadPage downloadPage;
+    private FileUploadPage uploadPage;
+    private MainPage mainPage;
+
+    @BeforeClass
+    public void testSetup () {
+        final String websiteLink = "http://the-internet.herokuapp.com/";
+        driver.get(websiteLink);
+        mainPage = new MainPage(driver);
+        downloadPage = new DownloadPage(driver);
+        uploadPage = new FileUploadPage(driver);
+    }
+
+    @Test
+    public void testFileDownload () throws InterruptedException {
+        mainPage.clickLink("File Download");
+        fileName = downloadPage.getDownloadLinkText();
+        downloadPage.downloadFile();
+        Assert.assertTrue(downloadPage.checkFileDownload(fileName));
+        driver.navigate().back();
+    }
+
+    @Test
+    public void testFileUpload () {
+        mainPage.clickLink("File Upload");
+        uploadPage.uploadFile(fileName);
+        Assert.assertEquals("File Uploaded!", uploadPage.successHeader());
+        Assert.assertEquals(fileName, uploadPage.uploadedFileName());
+    }
+}
