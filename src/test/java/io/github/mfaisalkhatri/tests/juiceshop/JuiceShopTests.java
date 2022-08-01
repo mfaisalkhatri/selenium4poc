e93a02f01bdb081b39e9db855b57e88dd40dbd50
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2022-08-01 21:34:57 +0530

    removed localhost website link from selenium grid tests and updated global link

--- src/test/java/io/github/mfaisalkhatri/tests/juiceshop/JuiceShopTests.java
+++ src/test/java/io/github/mfaisalkhatri/tests/juiceshop/JuiceShopTests.java
@@ -41,15 +41,15 @@ public class JuiceShopTests extends BaseSuiteSetup {
 
     @BeforeClass
     public void setupTests () {
-        // final String websiteLink = "https://juice-shop.herokuapp.com/#/";
-     //   final String dockerWebsiteLink = "http://host.docker.internal:3000/#/";
+         final String globalWebsiteLink = "https://juice-shop.herokuapp.com/#/";
+       //final String dockerWebsiteLink = "http://host.docker.internal:3000/#/";
         final String websiteLink = "http://localhost:3000/#/";
-//        System.out.println (getDriver ().toString ());
-//        if (getDriver ().toString ().contains ("RemoteWebDriver")) {
-//            getDriver ().get (dockerWebsiteLink);
-//        } else {
+        System.out.println (getDriver ().toString ());
+        if (getDriver ().toString ().contains ("RemoteWebDriver")) {
+            getDriver ().get (globalWebsiteLink);
+        } else {
             getDriver ().get (websiteLink);
-        //}
+        }
         this.mainPage = new MainPage ();
         this.loginPage = new LoginPage ();
         this.productPage = new ProductPage ();
