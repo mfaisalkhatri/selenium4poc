3b5f6d9f4f9384af862cb67fd590d270d7d8b78e
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2022-11-16 18:35:43 +0530

    added test for single dropdown list

--- src/test/java/io/github/mfaisalkhatri/pages/lambdatestseleniumplayground/DropdownPage.java
+++ src/test/java/io/github/mfaisalkhatri/pages/lambdatestseleniumplayground/DropdownPage.java
@@ -2,6 +2,9 @@ package io.github.mfaisalkhatri.pages.lambdatestseleniumplayground;
 
 import static io.github.mfaisalkhatri.drivers.DriverManager.getDriver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
@@ -12,20 +15,45 @@ import org.openqa.selenium.support.ui.Select;
  **/
 public class DropdownPage {
 
-    public void checkIfMultiple () {
-        singleSelectDropdownList ().isMultiple ();
+    public boolean checkIfMultiple () {
+        return singleSelectDropdownList ().isMultiple ();
     }
 
-    public void getOptions () {
-        singleSelectDropdownList ().getOptions ();
+    public ArrayList<String> expectedDropdownOptions () {
+        ArrayList<String> expectedOptions = new ArrayList<> ();
+        expectedOptions.add ("Please select");
+        expectedOptions.add ("Sunday");
+        expectedOptions.add ("Monday");
+        expectedOptions.add ("Tuesday");
+        expectedOptions.add ("Wednesday");
+        expectedOptions.add ("Thursday");
+        expectedOptions.add ("Friday");
+        expectedOptions.add ("Saturday");
+        return expectedOptions;
+    }
+
+    public ArrayList<String> getOptions () {
+        final List<WebElement> allOptions = singleSelectDropdownList ().getOptions ();
+        final int size = allOptions.size ();
+        ArrayList<String> options = new ArrayList<> ();
+        for (int i = 0; i < size; i++) {
+            options.add (allOptions.get (i)
+                .getText ());
+        }
+        return options;
+    }
+
+    public String getSelectedDropDownText () {
+        return getDriver ().findElement (By.cssSelector ("p.selected-value"))
+            .getText ();
+    }
+
+    public void selectDayByVisibleText (final String visibleText) {
+        singleSelectDropdownList ().selectByVisibleText (visibleText);
     }
 
     public void selectSingleDayIndex (final int index) {
         singleSelectDropdownList ().selectByIndex (index);
-    }
-
-    public void selectSingleDayOption (final String visibleText) {
-        singleSelectDropdownList ().selectByVisibleText (visibleText);
     }
 
     public void selectSingleDayValue (final String value) {
