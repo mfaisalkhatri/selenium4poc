7a6a89d3edbcb1d6531ab17962b31f5c1f8b531a
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2022-12-28 00:03:45 +0530

    refactored data table tests, removed sout and added logger info to print table records

--- src/test/java/io/github/mfaisalkhatri/pages/theinternet/DataTablesPage.java
+++ src/test/java/io/github/mfaisalkhatri/pages/theinternet/DataTablesPage.java
@@ -6,10 +6,14 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class DataTablesPage {
+
+    private static final Logger LOG = LogManager.getLogger ("DataTablesPage.class");
 
     public List<WebElement> columnHeaders () {
         return getDriver ().findElements (By.cssSelector ("#table1 > thead > tr > th"));
@@ -38,7 +42,7 @@ public class DataTablesPage {
             final List<WebElement> tableColumns = tableRows ().get (i)
                 .findElements (By.tagName ("td"));
             for (int j = 0; j < totalColumns (); j++) {
-                System.out.println (tableColumns.get (j)
+                LOG.info (tableColumns.get (j)
                     .getText ());
             }
         }
