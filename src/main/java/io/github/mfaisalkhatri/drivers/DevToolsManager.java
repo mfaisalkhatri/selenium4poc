09d887fcfcbf9940baf52f0cbe544115bc69c024
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2023-05-11 16:49:18 +0530

    Versions for Plugins and dependencies updated (#113)
    * updated selenium dependency version and surefire, compiler and jacoco plugin versions
    
    * updated selenium devtools version
    
    * log class for v113 correctly imported
    
    * added caps for acceptcerts for ltoption to fix the boolean value issue for running tests on lambdatest platform

--- src/main/java/io/github/mfaisalkhatri/drivers/DevToolsManager.java
+++ src/main/java/io/github/mfaisalkhatri/drivers/DevToolsManager.java
@@ -9,7 +9,7 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v101.log.Log;
+import org.openqa.selenium.devtools.v113.log.Log;
 
 /**
  * Created By Faisal Khatri on 21-03-2022
@@ -17,40 +17,40 @@ import org.openqa.selenium.devtools.v101.log.Log;
 @Builder
 public class DevToolsManager {
 
-    private static final Logger       LOG = LogManager.getLogger ("DevToolsSetup.class");
-    private static       ChromeDriver chromeDriver;
+    private static final Logger LOG = LogManager.getLogger("DevToolsSetup.class");
+    private static ChromeDriver chromeDriver;
 
-    public static void createDriver () {
-        final boolean isHeadless = Boolean.parseBoolean (
-            Objects.requireNonNullElse (System.getProperty ("headless"), "true"));
-        WebDriverManager.chromedriver ()
-            .setup ();
-        ChromeOptions options = new ChromeOptions ();
-        options.addArguments ("--no-sandbox");
-        options.addArguments ("--disable-dev-shm-usage");
-        options.addArguments ("--window-size=1050,600");
+    public static void createDriver() {
+        final boolean isHeadless = Boolean.parseBoolean(
+                Objects.requireNonNullElse(System.getProperty("headless"), "true"));
+        WebDriverManager.chromedriver()
+                .setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1050,600");
         if (isHeadless) {
-            options.addArguments ("--headless");
+            options.addArguments("--headless");
         }
-        options.addArguments ("--safebrowsing-disable-download-protection");
+        options.addArguments("--safebrowsing-disable-download-protection");
 
-        chromeDriver = new ChromeDriver (options);
-        DevTools chromeDevTools = chromeDriver.getDevTools ();
-        chromeDevTools.createSession ();
-        chromeDevTools.send (Log.enable ());
-        chromeDevTools.addListener (Log.entryAdded (), logEntry -> {
-            LOG.error (logEntry.getText ());
-            LOG.error (logEntry.getLevel ());
+        chromeDriver = new ChromeDriver(options);
+        DevTools chromeDevTools = chromeDriver.getDevTools();
+        chromeDevTools.createSession();
+        chromeDevTools.send(Log.enable());
+        chromeDevTools.addListener(Log.entryAdded(), logEntry -> {
+            LOG.error(logEntry.getText());
+            LOG.error(logEntry.getLevel());
         });
     }
 
-    public static ChromeDriver getDriver () {
+    public static ChromeDriver getDriver() {
         return chromeDriver;
     }
 
-    public static void quitDriver () {
+    public static void quitDriver() {
         if (null != chromeDriver) {
-            chromeDriver.quit ();
+            chromeDriver.quit();
         }
     }
 }
