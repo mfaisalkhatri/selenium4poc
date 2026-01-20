bd250de7a1fbb42598879997f1ff18030476e73b
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2026-01-20 15:16:57 +0530

    Moved all the tests to run on Chrome browser (#191)
    * replaced the firefox browser with chrome for all the tests
    
    * disabled log tests as the site is not working

--- src/test/java/io/github/mfaisalkhatri/pages/lambdatestseleniumplayground/MainPage.java
+++ src/test/java/io/github/mfaisalkhatri/pages/lambdatestseleniumplayground/MainPage.java
@@ -1,9 +1,13 @@
 package io.github.mfaisalkhatri.pages.lambdatestseleniumplayground;
 
+import static io.github.mfaisalkhatri.drivers.DriverManager.getDriver;
+
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
-import static io.github.mfaisalkhatri.drivers.DriverManager.getDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * @author Faisal Khatri
@@ -11,8 +15,14 @@ import static io.github.mfaisalkhatri.drivers.DriverManager.getDriver;
  **/
 public class MainPage {
 
+    private final WebDriverWait wait;
+
+    public MainPage () {
+        this.wait = new WebDriverWait (getDriver (), Duration.ofSeconds (20));
+    }
+
     public void clickLink (final String linkName) {
-        final WebElement link = getDriver ().findElement (By.linkText (linkName));
+        final WebElement link = this.wait.until (ExpectedConditions.elementToBeClickable (By.linkText (linkName)));
         link.click ();
     }
 
